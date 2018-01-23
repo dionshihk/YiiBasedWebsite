432e6466822bfa7cefd38350268ddc18139e48d8
Di Shi <dionbaby@icloud.com>
2018-01-23 15:57:19 +0800

    Initial Index

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+date_default_timezone_set("PRC");
+session_name("MaskV3SessionID");
+
+
+$yii=dirname(__FILE__).'/../YiiFramework/yiilite.php';
+$config=dirname(__FILE__).'/config.php';
+
+defined('YII_DEBUG') or define('YII_DEBUG',true);
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+
+require_once($yii);
+Yii::createWebApplication($config)->run();
