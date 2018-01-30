0b16e45d5c0417978ba3237aed6b1735b2aabba9
Di Shi <dionbaby@icloud.com>
2018-01-30 14:51:32 +0800

    Sign In CSS

--- protected/views/site/signinPopup.php
+++ protected/views/site/signinPopup.php
@@ -1,10 +1,11 @@
 <form id="loginForm" method="post">
     <p><?=$this->t('email')?> ››</p>
     <input name="LoginForm[name]" class="sys" id="sysLoginName" type="text">
-    <p style="position:relative"><?=$this->t('password')?> ››
+    <div style="position:relative">
+        <p><?=$this->t('password')?> ››</p>
+        <input name="LoginForm[password]" onkeyup="if(event.keyCode===13){systemLogin()}" id="sysLoginPass" type="password" class="sys">
         <a href="/resetPassword/forgot" class="link forgotPassword"><?=$this->t('account.1')?></a>
-    </p>
-    <input name="LoginForm[password]" onkeyup="if(event.keyCode===13){systemLogin()}" id="sysLoginPass" type="password" class="sys">
+    </div>
     <label class="tickLabel"><input type="checkbox" name="LoginForm[remember]"><?=$this->t('account.3')?></label>
     <div class="blackButton" onclick="systemLogin()"><?=$this->t('signin')?></div>
 </form>
